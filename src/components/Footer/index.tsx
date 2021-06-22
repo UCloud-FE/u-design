f29517f395fdcaee97113e56cde046c28aae55f4
WangLin <lin.wang@ucloud.cn>
2021-06-22 19:04:21 +0800

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,11 +4,23 @@ import * as styles from './styles.module.scss';
 const Index = () => {
     return (
         <footer className={styles.footer}>
-            <ul>
-                <li><a href="https://www.ucloud.cn" target="_blank">UCloud 官网</a></li>
-                <li><a href="https://docs.ucloud.cn" target="_blank">UCloud 文档中心</a></li>
-                <li><a href="https://console.ucloud.cn" target="_blank">UCloud 控制台</a></li>
-            </ul>
+            <div className={styles.links}>
+                <div>
+                    <h2>UCloud</h2>
+                    <ul>
+                        <li><a href="https://www.ucloud.cn" target="_blank">UCloud 官网</a></li>
+                        <li><a href="https://docs.ucloud.cn" target="_blank">UCloud 文档中心</a></li>
+                        <li><a href="https://console.ucloud.cn" target="_blank">UCloud 控制台</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>社区</h2>
+                    <ul>
+                        <li><a href="https://github.com/UCloud-FE/react-components" target="_blank">GitHub</a></li>
+                    </ul>
+                </div>
+            </div>
+
             <div className={styles.bottom}>
                 © {new Date().getFullYear()}, UCloud
             </div>
